849e1a75420e0a65e6555bb86c4f364d043dcbac
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-08 12:20:46 +0530

    Chat App design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
+        <div className="container">
+            <Sidebar />
+            <Chat />
+        </div>
       
     </div>
   )
