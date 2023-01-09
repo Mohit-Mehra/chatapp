5c1679c67b358294e5d181b5a6878e26ac6b0000
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-09 11:57:42 +0530

    Search Complete

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
-      
+    <div className='serach'>
+      <div className="searchForm">
+        <input type="text" placeholder='Find User'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userChatInfo">
+          <span>Mohit</span>
+        </div>
+      </div>
     </div>
   )
 }
