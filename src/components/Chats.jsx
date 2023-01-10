e2ec791ac4080e78206360d9680df5401ecfeb3f
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-10 21:04:23 +0530

    Chats Complete

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>
-      
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userChatInfo">
+          <span>Mohit</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userChatInfo">
+          <span>Mohit</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userChatInfo">
+          <span>Mohit</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
