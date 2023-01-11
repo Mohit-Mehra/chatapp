687a945089e607d35f15deebf90cabcb7e51fa28
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-11 11:22:28 +0530

    Chat Complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Cam from '../img/cam.png'
+import Add from '../img/plus.png'
+import More from '../img/option.png'
 const Chat = () => {
   return (
     <div className='chat'>
-      c
+      <div className="chatInfo">
+        <span>Mohit</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
