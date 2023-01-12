acc74a3a22f4d7c698bdc4226c90049b788a56bd
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-12 14:22:57 +0530

    frontend Complete

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      
+    <div className='messasge owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+      </div>
     </div>
   )
 }
