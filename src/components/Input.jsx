9a7c0ed31fa081172684400037bd754a50111edc
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-12 14:03:45 +0530

    input complete

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Img from '../img/add-image.png'
+import Attach from '../img/attachment.png'
 
 const Input = () => {
   return (
-    <div>
-      
+    <div className='input'>
+      <input type="text" placeholder='Tyoe Something....' />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
