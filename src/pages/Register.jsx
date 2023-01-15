16486ad684e06601b31fbd60e92b4797776270c0
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-15 17:44:40 +0530

    authentication

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import React, { useState } from 'react'
 import Add from "../img/addAvatar.png"
 
 function Register() {
+  const [err,setErr] = useState(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+    } catch (err) {
+      setErr(true)
+    }
+
+
+  }
   return (
     <div className='formContainer'>
       <div className="formWrapper">
         <span className="logo">MV Chat</span>
         <span className="title">Register</span>
-        <form>
-            <input type="text" placeholder='display name' />
-            <input type="email" placeholder='email' />
-            <input type="password" placeholder='password'/>
-            <input style={{display:"none"}} type="file" id='file' />
-            <label htmlFor="file">
-                <img src={Add} alt="" />
-                <span>Add an Avatar</span>
-            </label>
-            <button>Sign Up</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='display name' />
+          <input type="email" placeholder='email' />
+          <input type="password" placeholder='password' />
+          <input style={{ display: "none" }} type="file" id='file' />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an Avatar</span>
+          </label>
+          <button>Sign Up</button>
         </form>
         <p>You do have an account? Login</p>
       </div>
