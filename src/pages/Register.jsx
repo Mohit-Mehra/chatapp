5de5c84bfd97b1bd8e4168aa912cdcc03df25fd8
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-16 18:10:55 +0530

    upload photo

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,11 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {storage, auth } from "../firebase";
 import React, { useState } from 'react'
 import Add from "../img/addAvatar.png"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function Register() {
-  const [err,setErr] = useState(false)
+  const [err, setErr] = useState(false)
   const handleSubmit = async (e) => {
     e.preventDefault()
     const displayName = e.target[0].value
@@ -14,11 +15,27 @@ function Register() {
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+
+        (error) => {
+          setErr(true)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            await updateProfile(res.user,{
+              displayName,
+              photoURL:downloadURL,
+            })
+          });
+        }
+      );
     } catch (err) {
       setErr(true)
     }
-
-
   }
   return (
     <div className='formContainer'>
@@ -35,6 +52,7 @@ function Register() {
             <span>Add an Avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong!</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
